c5250fb27a031089b703e704e5cfb1b8c3de7953
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-04-05 15:03:41 -0300

    build part of the mobile portfolio popup

--- actions.js
+++ actions.js
@@ -1,8 +1,11 @@
 /* eslint-disable no-plusplus */
 const btnHamburger = document.getElementById('hamburger-menu');
 const mobileMenu = document.getElementById('mobile-menu');
+const mobilePortfolio = document.getElementById('mobile-portfolio');
 const btnCross = document.getElementById('onlycross');
+const btnCross2 = document.getElementById('onlycross2');
 const menuItem = document.querySelectorAll('.list-item');
+const btnProject1 = document.getElementById('Project1-btn');
 btnHamburger.addEventListener('click', () => {
   mobileMenu.style.display = 'block';
 });
@@ -14,3 +17,9 @@ for (let i = 0; i < menuItem.length; i++) {
     mobileMenu.style.display = 'none';
   });
 }
+btnCross2.addEventListener('click', () => {
+  mobilePortfolio.style.display = 'none';
+});
+btnProject1.addEventListener('click', () => {
+  mobilePortfolio.style.display = 'block';
+});
