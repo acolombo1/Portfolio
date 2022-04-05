117756b92ab7c49d20f58c1b758c23e3228c3682
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-04-05 15:36:12 -0300

    add blur filter, add gitignore .vscode

--- actions.js
+++ actions.js
@@ -5,6 +5,7 @@ const mobilePortfolio = document.getElementById('mobile-portfolio');
 const btnCross = document.getElementById('onlycross');
 const btnCross2 = document.getElementById('onlycross2');
 const menuItem = document.querySelectorAll('.list-item');
+const toBlur = document.querySelectorAll('.blur');
 const btnProject1 = document.getElementById('Project1-btn');
 btnHamburger.addEventListener('click', () => {
   mobileMenu.style.display = 'block';
@@ -19,7 +20,13 @@ for (let i = 0; i < menuItem.length; i++) {
 }
 btnCross2.addEventListener('click', () => {
   mobilePortfolio.style.display = 'none';
+  for (let i = 0; i < toBlur.length; i++) {
+    toBlur[i].style = 'filter: none;'
+  }
 });
 btnProject1.addEventListener('click', () => {
   mobilePortfolio.style.display = 'block';
+  for (let i = 0; i < toBlur.length; i++) {
+    toBlur[i].style = 'filter: blur(5px);'
+  }
 });
