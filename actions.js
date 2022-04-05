4eef9267b70642f5b2cb42b2757adfd0ec3ccd89
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-04-05 19:08:53 -0300

    make popup scrollable, add button links

--- actions.js
+++ actions.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable no-plusplus */
 const btnHamburger = document.getElementById('hamburger-menu');
 const mobileMenu = document.getElementById('mobile-menu');
@@ -21,12 +22,12 @@ for (let i = 0; i < menuItem.length; i++) {
 btnCross2.addEventListener('click', () => {
   mobilePortfolio.style.display = 'none';
   for (let i = 0; i < toBlur.length; i++) {
-    toBlur[i].style = 'filter: none;'
+    toBlur[i].style = 'filter: none;';
   }
 });
 btnProject1.addEventListener('click', () => {
   mobilePortfolio.style.display = 'block';
   for (let i = 0; i < toBlur.length; i++) {
-    toBlur[i].style = 'filter: blur(5px);'
+    toBlur[i].style = 'filter: blur(5px);';
   }
 });
